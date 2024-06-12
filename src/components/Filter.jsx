c106490825e9c768e276b0644a7b1668223c94f3
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-12 15:25:27 +0530

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -6,7 +6,7 @@ const Filter = ({ setRegion }) => {
         <div>
             <select className="filter-by-region"
                 onChange={(e) => { setRegion(e.target.value) }}>
-                <option hidden="">Filter by Region</option>
+                <option hidden>Filter by Region</option>
                 <option value="Africa">Africa</option>
                 <option value="Americas">America</option>
                 <option value="Asia">Asia</option>
